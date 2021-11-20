d799abc10ca45e09252025511eb7fd72b48d6e09
grsol <gr.solovyev@gmil.com>
2021-11-20 18:11:57 +0300

    добавил 2 часть

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -28,26 +28,17 @@ public class App
         driver.findElement(By.id("prependedInput")).sendKeys("Applanatest1");
         driver.findElement(By.id("prependedInput2")).sendKeys("Student2020!");
         driver.findElement(By.id("_submit")).click();
-        driver.findElement(By.xpath("//*[@id=\"main-menu\"]//span[text()='Проекты']")).click();
-        driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[3]/ul/li[4]/a/span")).click();
-        driver.findElement(By.xpath("//div[@class=\"pull-left btn-group icons-holder\"]/a")).click();
-        driver.findElement(By.xpath("//input[@name=\"crm_project[name]\"]")).sendKeys("Наименование");
-        driver.findElement(By.xpath("//a[@class=\"select2-choice select2-default\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"main-menu\"]//li[@class=\"dropdown first\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[1]/ul/li[4]/a/span")).click(); //  перешел в контактные лица
+        driver.findElement(By.xpath("//div[@class=\"pull-left btn-group icons-holder\"]/a")).click(); // нажал создать контакт
+        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div/div[1]//input[@name=\"crm_contact[lastName]\"]")).sendKeys("Иванов");
+        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div/div[2]/div[2]/input[@name=\"crm_contact[firstName]\"]")).sendKeys("Иван");
+        driver.findElement(By.xpath("//div[@class=\"company-container\"]/div")).click();
         driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul[2]/li[2]/div")).click();
-        driver.findElement(By.xpath("//div[@class=\"responsive-cell crm-block responsive-cell-no-blocks\"]//div[@class=\"select2-container select2\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul[2]/li[1]/div")).click();
-        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div[2]/div[5]/div[2]/div")).click();
-        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div[2]/div[5]/div[2]/div/select/option[2]")).click(); //выбор подразделения
-        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div[2]/div[6]/div[2]/div")).click();
-        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div[2]/div[6]//select/option[4]")).click(); // выбор куратора
-        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div[2]/div[7]/div[2]/div")).click();
-        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div[2]/div[7]/div[2]/div/select/option[3]")).click(); //выбор руководителя
-        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div[2]/div[8]/div[2]/div")).click();
-        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div[2]/div[8]/div[2]/div/select/option[3]")).click(); //выбор администратора
-        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div[2]/div[9]/div[2]/div")).click();
-        driver.findElement(By.xpath("//fieldset[@class=\"form-horizontal\"]/div[2]/div[9]/div[2]/div/select/option[3]")).click(); // выбор менеджера
-        driver.findElement(By.xpath("//div[@class=\"pull-right\"]/div/div[4]")).click();  // нажимаю Сохранить и выйти
-       // driver.quit();
+        driver.findElement(By.xpath("//div[@class=\"responsive-cell responsive-cell-no-blocks\"]//input[@name=\"crm_contact[jobTitle]\"]")).sendKeys("Охранник");
+        driver.findElement(By.xpath("//div[@class=\"navigation clearfix navbar-extra navbar-extra-right\"]/div/div[2]/div/div[4]/button")).click();
+
+
 
     }
 }
